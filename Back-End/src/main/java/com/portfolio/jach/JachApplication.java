05cbdf6985b89833330a5225f27fb9e04ca740ad
jaweropuntano <jorgeachavero@hotmail.com>
2023-05-11 18:11:24 -0300

    modificacion3

---
+++ Back-End/src/main/java/com/portfolio/jach/JachApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JachApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/portfolio/jach/JachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
