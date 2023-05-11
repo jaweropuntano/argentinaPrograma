6aa0fff736af602955901a821b4433e489cb01d2
jaweropuntano <jorgeachavero@hotmail.com>
2023-05-10 22:32:59 -0300

    commit

--- argentinaPrograma/src/app/app.module.ts
+++ argentinaPrograma/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
